0302a058b045a6998f81db5f4013c068ce56ff6d
Ragulsankar04 <ragulsankar2004@gmail.com>
2022-11-05 22:00:13 +0530

    sanity added

--- pages/redeem.js
+++ pages/redeem.js
@@ -1,5 +1,12 @@
 const redeem = () => {
-  return <div>redeem</div>
+  return (
+    <div className="master">
+      <div className="heading">Shopping</div>
+      <div className="card">Suede Sneakers </div>
+    </div>
+
+  )
+
 }
 
 export default redeem
