1624d83921b7d55358ad67b5ca20b891053fcb6b
Chanakyha <vetrichanakyha2003@gmail.com>
2022-11-06 15:49:06 +0530

    Added the Connection of Sanity to Frontend

--- sanity.js
+++ sanity.js
@@ -1,10 +1,13 @@
 import sanityClient from "@sanity/client";
 
+console.log(process.env.NEXT_PUBLIC_SANITY_TOKEN);
+
 const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2021-10-21",
-  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  useCdn: false,
 });
 
 export default client;
