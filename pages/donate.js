f7f7471e832ac4a30095c350e4574c946896734d
Ragulsankar04 <ragulsankar2004@gmail.com>
2022-11-06 07:54:33 +0530

    Sanity updated

--- pages/donate.js
+++ pages/donate.js
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react"
+import client from "../sanity"
+
 const donate = () => {
+  const [raiseFunds,setRaiseFunds] = useState(false)
+  
+
+useEffect(() => {
+  if (client) {
+
+
+  
+  const groq = `*[_type == "raise_funds"]{
+    _createdAt,name,description,donation_amount,email
+  }]`
+  client.fetch(groq).then((data) => {console.log(data);setRaiseFunds(data)})}
+    
+}, [])
+
+
   return (
     <div className="donate">
       <div className="title">
---
+++ sanity.js
@@ -0,0 +1,13 @@
+import sanityClient from '@sanity/client'
+
+
+
+const client = sanityClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2021-10-21', 
+  useCdn: true, 
+})
+
+
+export default sanityClient;
