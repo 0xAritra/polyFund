f7473ecf411e920569e5d09ecb3ec701034f56e9
Chanakyha <vetrichanakyha2003@gmail.com>
2022-11-06 10:40:51 +0530

    Getting the Data from from the sanity API

--- components/Header.js
+++ components/Header.js
@@ -1,49 +1,49 @@
-import Web3Modal from "web3modal"
-import { providers, Contract } from "ethers"
-import { useRef, useEffect, useState } from "react"
-import Landing from "./Landing"
-import Link from "next/link"
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { useRef, useEffect, useState } from "react";
+import Landing from "./Landing";
+import Link from "next/link";
 
 const Header = () => {
-  const [walletConnected, setWalletConnected] = useState(false)
-  const Web3ModalRef = useRef()
+  const [walletConnected, setWalletConnected] = useState(false);
+  const Web3ModalRef = useRef();
 
   const getProviderOrSigner = async (needSigner = false) => {
     try {
-      const provider = await Web3ModalRef.current.connect()
-      const web3Provider = new providers.Web3Provider(provider)
-      const { chainId } = await web3Provider.getNetwork()
+      const provider = await Web3ModalRef.current.connect();
+      const web3Provider = new providers.Web3Provider(provider);
+      const { chainId } = await web3Provider.getNetwork();
       // console.log(chainId)
       if (chainId !== 80001) {
-        window.alert("Change to Mumbai network")
-        throw new Error("Change to Mumbai network")
+        // window.alert("Change to Mumbai network")
+        // throw new Error("Change to Mumbai network");
       }
       if (needSigner) {
-        const signer = web3Provider.getSigner()
-        return signer
+        const signer = web3Provider.getSigner();
+        return signer;
       }
-      return web3Provider
+      return web3Provider;
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   const renderButton = () => {
     if (walletConnected) {
-      return <div></div>
+      return <div></div>;
     } else {
-      return <button onClick={connectWallet}>Connect Wallet</button>
+      return <button onClick={connectWallet}>Connect Wallet</button>;
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
-      await getProviderOrSigner()
-      setWalletConnected(true)
+      await getProviderOrSigner();
+      setWalletConnected(true);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     if (!walletConnected) {
@@ -51,10 +51,10 @@ const Header = () => {
         network: "maticmum",
         providerOptions: {},
         disableInjectedProvider: false,
-      })
-      connectWallet()
+      });
+      connectWallet();
     }
-  }, [walletConnected])
+  }, [walletConnected]);
 
   return (
     <div className="navbar">
@@ -77,7 +77,7 @@ const Header = () => {
       </div>
       {renderButton()}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/donate.js
+++ pages/donate.js
@@ -1,25 +1,22 @@
-import Card from "../components/Card"
+import Card from "../components/Card";
 
-import { useEffect, useState } from "react"
-import client from "../sanity"
+import { useEffect, useState } from "react";
+import client from "../sanity";
 
 const donate = () => {
+  const [raiseFunds, setRaiseFunds] = useState([]);
 
-  const [raiseFunds,setRaiseFunds] = useState(false)
-  
-
-useEffect(() => {
-  if (client) {
-
-
-  
-  const groq = `*[_type == "raise_funds"]{
-    _createdAt,name,description,donation_amount,email
-  }]`
-  client.fetch(groq).then((data) => {console.log(data);setRaiseFunds(data)})}
-    
-}, [])
-
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          "https://v4f5v3wm.api.sanity.io/v2021-10-21/data/query/production?query=%0A*%5B_type%3D%3D%22raise_funds%22%5D%0A%0A"
+        )
+      ).json();
+      setRaiseFunds(data.result);
+      console.log(data.result);
+    })();
+  }, []);
 
   return (
     <div className="donate">
@@ -32,28 +29,18 @@ useEffect(() => {
           tenetur labore.
         </p>
       </div>
-      <Card
-        name='NIGERIA FLOOD RELEIF FUND'
-        description='Communities across Nigeria are experiencing the worst floods in at least 10 years. More than 600 people have been killed and 2.8 million people have been displaced from their homes. The floods are striking during a difficult period in Nigeria with soaring inflation and a growing risk of catastrophic levels of hunger. Your donation to the Nigeria Flood Relief Fund will provide immediate and long-term relief in affected communities.'
-        amount= '10 MATIC'
-      />
-      <Card 
-        name='UKRAINE WAR CRISIS'
-        description='The war in Ukraine is having a devastating impact on the country’s 7.5 million children. Humanitarian needs are multiplying by the hour as the fighting continues. Children continue to be killed, wounded and deeply traumatized by the violence all around them. Families are terrified, in shock, and desperate for safety. UNICEF is working with partners to reach vulnerable children and families with essential services – including health, education, protection, water and sanitation – as well as life-saving supplies.'
-        amount= '10 MATIC'
-      />
-      <Card 
-        name='HAIITI FUND '
-        description='Millions of Haitians are confronting a complex crisis driven by political and economic turmoil, leading to violence and food and water scarcity. More than 19,000 people in Haiti are experiencing hunger at a catastrophic level. Another 4 million people are facing severe food insecurity. Your donation to the Haiti Crisis Relief Fund will provide emergency assistance to people in need and support long-term response efforts led by local organizations.'
-        amount= '10 MATIC'
-      />
-      <Card
-        name='UKRAINE WAR CRISIS'
-        description='Communities across Nigeria are experiencing the worst floods in at least 10 years. More than 600 people have been killed and 2.8 million people have been displaced from their homes. The floods are striking during a difficult period in Nigeria with soaring inflation and a growing risk of catastrophic levels of hunger. Your donation to the Nigeria Flood Relief Fund will provide immediate and long-term relief in affected communities.'
-        amount= '10 MATIC'
-      />
+      {raiseFunds?.map((fund, index) => {
+        return (
+          <Card
+            key={index}
+            name={fund.name}
+            description={fund.description}
+            amount={fund.donation_amount}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default donate
+export default donate;
